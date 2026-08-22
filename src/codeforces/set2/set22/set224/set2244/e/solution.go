a78a7bd23a0e2f20c3cf644105b4b101d03c539a
bruce <wang.senyuan@gmail.com>
2026-08-22 15:54:19 +0800

    solved 2244 e

--- src/codeforces/set2/set22/set224/set2244/e/solution.go
+++ src/codeforces/set2/set22/set224/set2244/e/solution.go
@@ -39,7 +39,62 @@ func readQueries(reader *bufio.Reader, q int) [][3]int {
 }
 
 func solve(s string, queries [][3]int) []string {
-	// TODO: solve by hand first.
-	_ = s
-	return make([]string, len(queries))
+	n := len(s)
+	todo := make([][]int, n)
+	for i, cur := range queries {
+		r := cur[1] - 1
+		todo[r] = append(todo[r], i)
+	}
+
+	flag := make([]int, len(queries))
+
+	val := make([]int, n)
+
+	pref := make([]int, n)
+
+	play := func(pat string, d int) {
+		// 如果 pat是 0101
+		clear(pref)
+		clear(val)
+		var cnt int
+		for i := range n {
+			x := int(s[i] - '0')
+			y := int(pat[i&1] - '0')
+			if x != y {
+				val[i] = 1
+				// 这里必须要翻转一次
+				if i == 0 || val[i-1] == 0 {
+					cnt++
+				}
+			}
+			pref[i] = cnt
+			for _, j := range todo[i] {
+				l, k := queries[j][0]-1, queries[j][2]
+				k1 := cnt
+				if l > 0 {
+					k1 -= pref[l-1]
+				}
+				if val[l] > 0 && l > 0 && val[l-1] == 1 {
+					k1++
+				}
+				if k1 <= k {
+					flag[j] |= d
+				}
+			}
+		}
+	}
+	play("01", 1)
+	play("10", 2)
+
+	ans := make([]string, len(queries))
+
+	for i := range flag {
+		if flag[i] > 0 {
+			ans[i] = "YES"
+		} else {
+			ans[i] = "NO"
+		}
+	}
+
+	return ans
 }
