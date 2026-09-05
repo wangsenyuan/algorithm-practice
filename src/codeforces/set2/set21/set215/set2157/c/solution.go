c9a8e5f4e5dbbe81460b60e5422365162d8ef065
bruce <wang.senyuan@gmail.com>
2026-09-05 19:53:59 +0800

    aa

--- src/codeforces/set2/set21/set215/set2157/c/solution.go
+++ src/codeforces/set2/set21/set215/set2157/c/solution.go
@@ -14,25 +14,57 @@ func main() {
 	var tc int
 	fmt.Fscan(reader, &tc)
 	for range tc {
-		res := drive(reader)
+		res, _, _ := drive(reader)
 		s := fmt.Sprintf("%v", res)
 		fmt.Fprintln(writer, s[1:len(s)-1])
 	}
 }
 
-func drive(reader *bufio.Reader) []int {
+func drive(reader *bufio.Reader) ([]int, int, [][3]int) {
 	var n, k, q int
 	fmt.Fscan(reader, &n, &k, &q)
 	queries := make([][3]int, q)
 	for i := range queries {
 		fmt.Fscan(reader, &queries[i][0], &queries[i][1], &queries[i][2])
 	}
-	return solve(n, k, queries)
+	return solve(n, k, queries), k, queries
 }
 
 func solve(n, k int, queries [][3]int) []int {
-	// TODO
-	_ = k
-	_ = queries
-	return make([]int, n)
+	mark := make([]int, n+1)
+	mark2 := make([]int, n+1)
+	for _, cur := range queries {
+		l, r := cur[1]-1, cur[2]-1
+
+		if cur[0] == 1 {
+			mark[l]++
+			mark[r+1]--
+		} else {
+			mark2[l]++
+			mark2[r+1]--
+		}
+	}
+	for i := 1; i < n; i++ {
+		mark[i] += mark[i-1]
+		mark2[i] += mark2[i-1]
+	}
+
+	var arr []int
+	res := make([]int, n)
+	for i := range n {
+		if mark[i] > 0 {
+			res[i] = k + 1
+			if mark2[i] == 0 {
+				res[i] = k
+			}
+		} else if mark2[i] > 0 {
+			arr = append(arr, i)
+		}
+	}
+
+	for i, j := range arr {
+		res[j] = i % k
+	}
+
+	return res
 }
--- src/codeforces/set2/set21/set215/set2157/c/solution_test.go
+++ src/codeforces/set2/set21/set215/set2157/c/solution_test.go
@@ -7,13 +7,34 @@ import (
 	"testing"
 )
 
-func runSample(t *testing.T, s string, expect []int) {
+func runSample(t *testing.T, s string) {
 	t.Helper()
-	t.Skip("solve TODO")
 	reader := bufio.NewReader(strings.NewReader(s))
-	res := drive(reader)
-	if !slices.Equal(res, expect) {
-		t.Fatalf("Sample expect %v, but got %v", expect, res)
+	res, k, queries := drive(reader)
+	n := len(res)
+	freq := make([]int, n+1)
+	for _, cur := range queries {
+		l, r := cur[1]-1, cur[2]-1
+		if cur[0] == 1 {
+			w := slices.Min(res[l : r+1])
+			if w != k {
+				t.Fatalf("Sample result %v, is invalid", res)
+			}
+		} else {
+			clear(freq)
+			var mex int
+			for i := l; i <= r; i++ {
+				if res[i] < n {
+					freq[res[i]]++
+				}
+				for freq[mex] > 0 {
+					mex++
+				}
+			}
+			if mex != k {
+				t.Fatalf("Sample result %v, is invalid", res)
+			}
+		}
 	}
 }
 
@@ -21,25 +42,25 @@ func TestSample1(t *testing.T) {
 	runSample(t, `6 2 2
 1 1 3
 2 2 6
-`, []int{2, 5, 4, 3, 0, 1})
+`)
 }
 
 func TestSample2(t *testing.T) {
 	runSample(t, `3 3 1
 2 1 3
-`, []int{2, 0, 1})
+`)
 }
 
 func TestSample3(t *testing.T) {
 	runSample(t, `3 3 2
 1 1 1
 1 3 3
-`, []int{3, 3, 3})
+`)
 }
 
 func TestSample4(t *testing.T) {
 	runSample(t, `3 2 2
 2 1 2
 2 2 3
-`, []int{1, 0, 1})
+`)
 }
