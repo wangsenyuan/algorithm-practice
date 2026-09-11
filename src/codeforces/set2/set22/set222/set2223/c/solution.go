beaa584bd649538af7a6480ad7508235778f5a07
bruce <wang.senyuan@gmail.com>
2026-09-11 13:44:38 +0800

    aaa

--- src/codeforces/set2/set22/set222/set2223/c/solution.go
+++ src/codeforces/set2/set22/set222/set2223/c/solution.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -22,15 +21,15 @@ func main() {
 func drive(reader *bufio.Reader) string {
 	var n, q int
 	fmt.Fscan(reader, &n, &q)
-	f := make([]int, n+1)
-	for u := 2; u <= n; u++ {
+	f := make([]int, n-1)
+	for u := range n - 1 {
 		fmt.Fscan(reader, &f[u])
 	}
-	l := make([]int, n+1)
-	for u := 2; u <= n; u++ {
+	l := make([]int, n-1)
+	for u := range n - 1 {
 		fmt.Fscan(reader, &l[u])
 	}
-	ms := make([]int64, q)
+	ms := make([]int, q)
 	for i := range q {
 		fmt.Fscan(reader, &ms[i])
 	}
@@ -38,19 +37,77 @@ func drive(reader *bufio.Reader) string {
 }
 
 func formatInts(a []int) string {
-	var b strings.Builder
-	for i, x := range a {
-		if i > 0 {
-			b.WriteByte(' ')
-		}
-		fmt.Fprintf(&b, "%d", x)
-	}
-	return b.String()
+	s := fmt.Sprintf("%v", a)
+	return s[1 : len(s)-1]
 }
 
-func solve(f, l []int, ms []int64) []int {
-	// TODO
-	_ = f
-	_ = l
-	return make([]int, len(ms))
+const inf = 1e18
+
+func solve(f, l []int, ms []int) []int {
+	n := len(f) + 1
+	g := make([][]int, n)
+	dist := make([]int, n)
+
+	for i := 1; i < n; i++ {
+		g[f[i-1]-1] = append(g[f[i-1]-1], i)
+		dist[i] = dist[f[i-1]-1] + l[i-1]
+	}
+
+	ans := make([]int, len(ms))
+
+	var dfs func(u int, todo []int, rem int, M int)
+
+	dfs = func(u int, todo []int, rem int, M int) {
+		if len(g[u]) == 0 {
+			for _, i := range todo {
+				ans[i] = u + 1
+			}
+			return
+		}
+		if M > inf || M%len(g[u]) == 0 {
+			j := (rem + dist[u]) % len(g[u])
+			v := g[u][j]
+			dfs(v, todo, rem, M)
+			return
+		}
+
+		next := make([][]int, len(g[u]))
+		for _, i := range todo {
+			j := (ms[i] + dist[u]) % len(g[u])
+			next[j] = append(next[j], i)
+		}
+
+		M = lcm(M, len(g[u]))
+
+		for j, newTodo := range next {
+			if newTodo != nil {
+				dfs(g[u][j], newTodo, ms[newTodo[0]]%M, M)
+			}
+		}
+	}
+
+	todo := make([]int, len(ms))
+	for i := range todo {
+		todo[i] = i
+	}
+
+	dfs(0, todo, 0, 1)
+
+	return ans
+}
+
+func gcd(a int, b int) int {
+	for b > 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func lcm(a int, b int) int {
+	c := gcd(a, b)
+	a /= c
+	if a > inf/b {
+		return inf + 1
+	}
+	return a * b
 }
